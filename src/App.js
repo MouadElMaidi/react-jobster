ab1daf2797c6821daa7660f099991cbc3de29c5d
MouadElMaidi <mouadlmaidi@gmail.com>
2023-10-01 20:23:06 +0100

    Added router with different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Landing, Dashboard, Register, Error } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
@@ -22,7 +23,9 @@ const Landing = () => {
             quae commodi. Adipisci animi praesentium laboriosam reiciendis iure
             maiores deserunt impedit.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login / Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
