ea3dc6612d5e479d51bf67a8719d05104f5f1658
MouadElMaidi <mouadlmaidi@gmail.com>
2023-10-01 19:56:46 +0100

    Added landing page with css styles

---
+++ src/pages/Landing.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste labore
+            incidunt, nam voluptatibus corporis culpa odit impedit libero, natus
+            quae commodi. Adipisci animi praesentium laboriosam reiciendis iure
+            maiores deserunt impedit.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
