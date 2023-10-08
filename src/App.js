75b01ef003bff439ed665a13ef391e0c2b5ace2f
MouadElMaidi <mouadlmaidi@gmail.com>
2023-10-08 21:20:58 +0100

    Finished the sharedlayout component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { Landing, Register, Error } from "./pages";
-
+import { Landing, Register, Error, ProtectedRoute } from "./pages";
 import {
   SharedLayout,
   Stats,
@@ -16,7 +15,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
